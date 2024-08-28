ad25e7bc3fb8e66c95850d87c92f2d89788b3aa9
Charles Chien <charleschien101@gmail.com>
2024-08-27 19:49:15 -0700

    Fixed package name

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, str(HERE / "extensions"))
 
 # NOTE: If you installed your project in editable mode, this might be stale.
 #       If this is the case, reinstall it to refresh the metadata
-info = metadata("compass-sc")
+info = metadata("Compass")
 project_name = info["Name"]
 author = info["Author"]
 copyright = f"{datetime.now():%Y}, {author}."
