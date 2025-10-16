e9583a4b413b08b308b71f7f465f76c2a59a518a
Brandon Schellhaass <schel337@berkeley.edu>
2025-10-15 21:48:00 -0500

    made the version auto detected

--- crates/cplex-bindings/build.rs
+++ crates/cplex-bindings/build.rs
@@ -1,5 +1,6 @@
 use std::{collections::BTreeMap, env, path::PathBuf};
 
+use anyhow::Context;
 use bindgen::callbacks::{MacroParsingBehavior, ParseCallbacks};
 
 const FP_EXCLUDE_MACROS: &[&str] = &[
@@ -10,11 +11,8 @@ const FP_EXCLUDE_MACROS: &[&str] = &[
     "FP_NORMAL",
 ];
 
-// TODO: Make these constants configurable.
 // Default CPLEX installation path
-const CPLEX_PATH: &str = "/opt/ibm/ILOG";
-// The version I have installed
-const CPLEX_VERSION: &str = "CPLEX_Studio221";
+const DEFAULT_CPLEX_PATH: &str = "/opt/ibm/ILOG";
 
 #[derive(Debug)]
 pub struct FpMacroExcluder;
@@ -29,9 +27,58 @@ impl ParseCallbacks for FpMacroExcluder {
     }
 }
 
+fn get_cplex_version(cplex_path: &str) -> Result<String, anyhow::Error> {
+    const CPLEX_DIR_PREFIX: &str = "CPLEX_Studio";
+    if let Ok(ver) = std::env::var("CPLEX_VERSION") {
+        println!("Using CPLEX_VERSION from environment: {ver}");
+        return Ok(ver);
+    }
+    println!("Searching for CPLEX version in {cplex_path}");
+    let mut version: Option<String> = None;
+    let entries = std::fs::read_dir(cplex_path).with_context(|| format!("Reading from {cplex_path}"))?;
+    for entry in entries {
+        let entry = entry.context("Failed to read directory entry")?;
+        let file_os_name = entry.file_name();
+        let file_name = file_os_name.to_string_lossy();
+        if file_name.contains(CPLEX_DIR_PREFIX) {
+            println!("Found CPLEX version {file_name}");
+            match &version {
+                None => version = Some(file_name.into_owned()),
+                Some(prev) => {
+                    // Prefer any non-community edition
+                    // Prefer later versions
+                    let prev_is_community = prev.contains("Community");
+                    let curr_is_community = file_name.contains("Community");
+                    match (prev_is_community, curr_is_community) {
+                        (true, false) => version = Some(file_name.into_owned()),
+                        (false, true) => {}
+                        _ => {
+                            // Both are community or both are non-community
+                            // Prefer later version, based on lexicographical order.
+                            // E.G. CPLEX_Studio2212 > CPLEX_Studio221
+                            if file_name.as_ref() > prev.as_ref() {
+                                version = Some(file_name.into_owned());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    match version {
+        Some(version) => Ok(version),
+        None => return Err(anyhow::anyhow!("Could not find CPLEX version in {cplex_path}")),
+    }
+}
+
 pub fn main() {
+    let cplex_path = std::env::var("CPLEX_PATH").unwrap_or_else(|_| DEFAULT_CPLEX_PATH.to_string());
+    // Search for libraries in cplex path, or use the environent variable if set
+    let cplex_version = get_cplex_version(&cplex_path).unwrap();
+    println!("Using CPLEX version: {cplex_version}");
+    // Prefer CplexStudio
     println!(
-        "cargo:rustc-link-search={CPLEX_PATH}/{CPLEX_VERSION}/cplex/lib/x86-64_linux/static_pic/"
+        "cargo:rustc-link-search={cplex_path}/{cplex_version}/cplex/lib/x86-64_linux/static_pic/"
     );
 
     // Links with with /opt/ibm/ILOG/CPLEX_Studio221/cplex/lib/x86-64_linux/static_pic/libcplex.a
@@ -40,7 +87,7 @@ pub fn main() {
     let bindings = bindgen::Builder::default()
         .header("cplex_wrapper.h")
         .clang_arg(format!(
-            "-I{CPLEX_PATH}/{CPLEX_VERSION}/cplex/include/ilcplex/"
+            "-I{cplex_path}/{cplex_version}/cplex/include/ilcplex/"
         ))
         .parse_callbacks(Box::new(FpMacroExcluder))
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
